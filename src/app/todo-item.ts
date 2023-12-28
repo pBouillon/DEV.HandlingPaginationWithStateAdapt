b38ae1dfcbfa878b63db0133af243f57bd0d7750
pBouillon <pro.pierre.bouillon@protonmail.com>
2023-12-28 09:50:07 +0100

    Add the TodoItem adapters

--- src/app/todo-item.ts
+++ src/app/todo-item.ts
@@ -1,6 +1,18 @@
+import { createAdapter } from '@state-adapt/core';
+import { createEntityAdapter } from '@state-adapt/core/adapters';
+
 export interface TodoItem {
   id: number;
   title: string;
   description?: string;
   completed: boolean;
 }
+
+export const todoItemAdapter = createAdapter<TodoItem>()({
+  selectors: {
+    todoItem: (state) => state,
+  },
+});
+
+export const todoItemsAdapter =
+  createEntityAdapter<TodoItem>()(todoItemAdapter);
