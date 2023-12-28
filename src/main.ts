945b27a73479819502918dfc4c3197e3de5248a4
pBouillon <pro.pierre.bouillon@protonmail.com>
2023-12-28 09:30:45 +0100

    Add state-adapt

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+
+import { defaultStoreProvider } from '@state-adapt/angular';
+
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-  providers: [],
+  providers: [defaultStoreProvider],
 }).catch((err) => console.error(err));
